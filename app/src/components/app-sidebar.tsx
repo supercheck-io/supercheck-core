f33f21ed0445a93f1662dbfd593ba461f7f0e5a1
krish-kant <krishna.200587@gmail.com>
2025-10-21 18:47:24 +0530

    chore(version): update badge to v1.1.5-beta.22

--- app/src/components/app-sidebar.tsx
+++ app/src/components/app-sidebar.tsx
@@ -217,7 +217,7 @@ const data = {
       title: "Docs",
       url: "https://github.com/supercheck-io/supercheck",
       icon: BookOpenText,
-      badge: "v1.1.5-beta.21",
+      badge: "v1.1.5-beta.22",
     },
     // {
     //   title: "Changelog",
